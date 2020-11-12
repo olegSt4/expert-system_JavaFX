70a67b7e91ad8aec5ca901933fce9096f0aaf938
olegSt4 <stefura2010@ukr.net>
2020-11-12 20:10:44 +0200

    feat: Specialization class implemented

--- src/main/java/lab3/models/Specialization.java
+++ src/main/java/lab3/models/Specialization.java
@@ -1,4 +1,11 @@
 package lab3.models;
 
 public class Specialization {
+    public final int id;
+    public final String name;
+
+    public Specialization(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
