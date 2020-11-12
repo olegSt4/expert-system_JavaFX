7f2d7e5e2c1dad928269abe323a4e58efc98189a
olegSt4 <stefura2010@ukr.net>
2020-11-12 16:54:20 +0200

    feat: InputValidator class added

---
+++ src/main/java/lab3/logic/InputValidator.java
@@ -0,0 +1,8 @@
+package lab3.logic;
+
+public class InputValidator {
+    public static String[] getValidSkillsArray(String input) {
+        String[] skillsArray = input.split(" ");
+        return skillsArray;
+    }
+}
