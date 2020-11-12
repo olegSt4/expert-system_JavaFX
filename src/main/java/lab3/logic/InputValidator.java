53fae5735081f62da3e91f5a68c0ff3787680e3b
olegSt4 <stefura2010@ukr.net>
2020-11-12 17:30:11 +0200

    feat(InputValidator): getValidSkillsArray method implemented

--- src/main/java/lab3/logic/InputValidator.java
+++ src/main/java/lab3/logic/InputValidator.java
@@ -1,8 +1,21 @@
 package lab3.logic;
 
+import java.util.Iterator;
+
 public class InputValidator {
-    public static String[] getValidSkillsArray(String input) {
-        String[] skillsArray = input.split(" ");
+    public static String[] getValidSkillsArray(String input) throws IllegalArgumentException{
+        if (!input.matches("[a-zA-Z0-9.,+#/ ]+")) {
+            throw new IllegalArgumentException("Only numbers, letters or on of the symbols [.,+#/] are allowed!");
+        }
+
+        String[] skillsArray = input.split(",");
+        for (int i = 0; i < skillsArray.length; i++) {
+            skillsArray[i] = skillsArray[i]
+                                .replaceAll(",", "")
+                                .trim()
+                                .toLowerCase();
+            System.out.println(skillsArray[i]);
+        }
         return skillsArray;
     }
 }
