edf6f104da2867369cda96a922b73d55c326dc6f
olegSt4 <stefura2010@ukr.net>
2020-11-12 20:10:22 +0200

    feat: Skill class implemented

--- src/main/java/lab3/models/Skill.java
+++ src/main/java/lab3/models/Skill.java
@@ -1,4 +1,13 @@
 package lab3.models;
 
 public class Skill {
+    public final int id;
+    public final String name;
+    public final int complexity;
+
+    public Skill(int id, String name, int complexity) {
+        this.id = id;
+        this.name = name;
+        this.complexity = complexity;
+    }
 }
