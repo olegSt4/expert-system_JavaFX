b43bc37c1ff1930e0f9db352ddf444afc0bb49c2
olegSt4 <stefura2010@ukr.net>
2020-11-13 17:11:37 +0200

    feat(Skill): Object-methods overriden

--- src/main/java/lab3/models/Skill.java
+++ src/main/java/lab3/models/Skill.java
@@ -1,6 +1,6 @@
 package lab3.models;
 
-public class Skill {
+public class Skill implements Cloneable{
     public final int id;
     public final String name;
     public final int complexity;
@@ -9,5 +9,26 @@ public class Skill {
         this.id = id;
         this.name = name;
         this.complexity = complexity;
+    }
+
+    public Skill clone() {
+        return new Skill(id, name, complexity);
+    }
+
+    public boolean equals(Object other) {
+        if (other.getClass() != this.getClass()) {
+            return false;
+        }
+
+        Skill otherSkill = (Skill) other;
+        if (otherSkill.id != id || !otherSkill.name.equals(name) || otherSkill.complexity != complexity) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public int hashCode() {
+        return id;
     }
 }
