8555494dca335f1db620d623897bcbc8731c6212
olegSt4 <stefura2010@ukr.net>
2020-11-13 17:15:04 +0200

    feat(MainWindowController): basic implementation

--- src/main/java/lab3/controllers/MainWindowController.java
+++ src/main/java/lab3/controllers/MainWindowController.java
@@ -1,16 +1,41 @@
 package lab3.controllers;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import lab3.logic.ExpertSimulator;
+import lab3.logic.InputValidator;
+import lab3.models.SpecializationSummary;
+
+import java.util.List;
 
 public class MainWindowController
 {
     @FXML TextField skillsLine;
 
+    private ExpertSimulator expertSimulator;
+
+    public void initialize() {
+        expertSimulator = new ExpertSimulator(new DBController());
+    }
+
     public void acceptSkills() {
+
         String input = skillsLine.getText();
-        System.out.println(input);
+        String[] skillsArray;
+        try {
+            skillsArray = InputValidator.getValidSkillsArray(input);
+        } catch (IllegalArgumentException ex) {
+            System.out.println(ex.getMessage());
+            return;
+        }
+
+
+        List<SpecializationSummary> topSpecializations =  expertSimulator.getSpecializationsBySkillsArray(skillsArray);
+        for (SpecializationSummary ss : topSpecializations) {
+            System.out.println(ss.getSpecialization().name + " - " + (int) ss.getPotentionalInPercentages() + "%");
+        }
+        System.out.println();
+
         skillsLine.clear();
     }
 
