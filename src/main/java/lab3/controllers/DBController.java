62a47b39bf04d809a3346afc1f2b1f7a5d9a3888
olegSt4 <stefura2010@ukr.net>
2020-11-15 20:29:13 +0200

    refactor(DBController): redundant call for ClassLoader moved to constructor

--- src/main/java/lab3/controllers/DBController.java
+++ src/main/java/lab3/controllers/DBController.java
@@ -20,34 +20,33 @@ public class DBController {
     private Map<Skill, Set<Position>> relevantPositionsCache = new HashMap<>();
     private Map<String, PositionSummary> positionSummariesCache = new HashMap<>();
 
+    public DBController() throws ClassNotFoundException{
+        Class.forName(JDBC_DRIVER);
+    }
+
     public List<Skill> getSkillsListByStringArray(String[] skillsName) {
         List<Skill> skillsList = new ArrayList<>();
-        try {
-            Class.forName(JDBC_DRIVER);
+
+        String SQLQuery = "SELECT s.id FROM skills s where s.name = ?";
+        try (Connection connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USER, DATABASE_PASSWORD);
+             PreparedStatement ps = connection.prepareStatement(SQLQuery)) {
 
             for (String skillName : skillsName) {
                 if (skillsCache.containsKey(skillName)) {
                     skillsList.add(skillsCache.get(skillName));
                 } else {
-                    String SQLQuery = "SELECT s.id FROM skills s where s.name = ?";
-                    try (Connection connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USER, DATABASE_PASSWORD);
-                        PreparedStatement ps = connection.prepareStatement(SQLQuery)) {
-                        ps.setString(1, skillName);
+                    ps.setString(1, skillName);
 
-                        ResultSet resultSet = ps.executeQuery();
-                        resultSet.next();
-
+                    ResultSet resultSet = ps.executeQuery();
+                    if (resultSet.next()) {
                         Skill newSkill = new Skill(resultSet.getInt(1), skillName);
                         skillsCache.put(skillName, newSkill);
 
                         skillsList.add(newSkill);
-                    } catch (SQLException ex) {
-                        ex.printStackTrace();
                     }
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            System.out.println("There is a problem with driver class!");
+        } catch (SQLException ex) {
             ex.printStackTrace();
         }
 
@@ -57,51 +56,42 @@ public class DBController {
     public Set<Position> getRelevantPositionsForSkills(List<Skill> skills) {
         Set<Position> relevantPositions = new HashSet<>();
 
-        try {
-            Class.forName(JDBC_DRIVER);
-
+        String SQLQuery = "SELECT DISTINCT p.id, p.name, p.image FROM positions p " +
+                "JOIN pos_requirements pr ON p.id = pr.pos_id " +
+                "JOIN skills sk ON sk.id = pr.skill_id " +
+                "WHERE sk.name = ?";
+        try (Connection connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USER, DATABASE_PASSWORD);
+             PreparedStatement ps = connection.prepareStatement(SQLQuery)) {
             for (Skill skill : skills) {
                 if (relevantPositionsCache.containsKey(skill)) {
                     relevantPositions.addAll(relevantPositionsCache.get(skill));
                 } else {
-                    String SQLQuery = "SELECT DISTINCT p.id, p.name, p.image FROM positions p " +
-                            "JOIN pos_requirements pr ON p.id = pr.pos_id " +
-                            "JOIN skills sk ON sk.id = pr.skill_id " +
-                            "WHERE sk.name = ?";
-                    try (Connection connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USER, DATABASE_PASSWORD);
-                        PreparedStatement ps = connection.prepareStatement(SQLQuery)) {
-                        ps.setString(1, skill.name);
+                    ps.setString(1, skill.name);
 
-                        ResultSet resultSet = ps.executeQuery();
+                    ResultSet resultSet = ps.executeQuery();
 
-                        Set<Position> skillRelevantPositions = new HashSet<>();
-                        while(resultSet.next()) {
-                            int posId = resultSet.getInt(1);
-                            String posName = resultSet.getString(2);
-                            InputStream imageByteStream = new ByteArrayInputStream(resultSet.getBytes(3));
-                            Image posImage = new Image(imageByteStream);
+                    Set<Position> skillRelevantPositions = new HashSet<>();
+                    while(resultSet.next()) {
+                        int posId = resultSet.getInt(1);
+                        String posName = resultSet.getString(2);
+                        InputStream imageByteStream = new ByteArrayInputStream(resultSet.getBytes(3));
+                        Image posImage = new Image(imageByteStream);
 
-                            Position newPosition = new Position(posId, posName, posImage);
-                            skillRelevantPositions.add(newPosition);
-                        }
+                        Position newPosition = new Position(posId, posName, posImage);
+                        skillRelevantPositions.add(newPosition);
+                    }
 
-                        if (skillRelevantPositions.size() > 0) {
-                            relevantPositions.addAll(skillRelevantPositions);
-                            relevantPositionsCache.put(skill, skillRelevantPositions);
-                        }
-
-
-                    } catch (SQLException ex) {
-                        ex.printStackTrace();
+                    if (skillRelevantPositions.size() > 0) {
+                        relevantPositions.addAll(skillRelevantPositions);
+                        relevantPositionsCache.put(skill, skillRelevantPositions);
                     }
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            System.out.println("There is a problem with driver class!");
+        } catch (SQLException ex) {
             ex.printStackTrace();
         }
 
-          return relevantPositions;
+        return relevantPositions;
        }
 
     public PositionSummary getPositionSummary(Position position)  {
@@ -109,45 +99,37 @@ public class DBController {
             return positionSummariesCache.get(position.name);
         } else {
             PositionSummary positionSummary = new PositionSummary(position);
-            try {
-                Class.forName(JDBC_DRIVER);
 
-                String SQLQuery = "SELECT sk.id, sk.name, pr.worth FROM positions p " +
-                        "JOIN pos_requirements pr ON p.id = pr.pos_id " +
-                        "JOIN skills sk ON sk.id = pr.skill_id " +
-                        "WHERE p.name = ?";
-                try(Connection connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USER, DATABASE_PASSWORD);
-                    PreparedStatement preparedStatement = connection.prepareStatement(SQLQuery)) {
-                    preparedStatement.setString(1, position.name);
+            String SQLQuery = "SELECT sk.id, sk.name, pr.worth FROM positions p " +
+                    "JOIN pos_requirements pr ON p.id = pr.pos_id " +
+                    "JOIN skills sk ON sk.id = pr.skill_id " +
+                    "WHERE p.name = ?";
+            try(Connection connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USER, DATABASE_PASSWORD);
+                PreparedStatement preparedStatement = connection.prepareStatement(SQLQuery)) {
+                preparedStatement.setString(1, position.name);
 
-                    Map<Skill, Double> worthOfSkills = new HashMap<>();
-                    ResultSet resultSet = preparedStatement.executeQuery();
-                    while (resultSet.next()) {
-                        int skillId = resultSet.getInt(1);
-                        String skillName = resultSet.getString(2);
-                        double skillWorth = resultSet.getDouble(3);
-                        Skill newSkill = new Skill(skillId, skillName);
+                Map<Skill, Double> worthOfSkills = new HashMap<>();
+                ResultSet resultSet = preparedStatement.executeQuery();
+                while (resultSet.next()) {
+                    int skillId = resultSet.getInt(1);
+                    String skillName = resultSet.getString(2);
+                    double skillWorth = resultSet.getDouble(3);
+                    Skill newSkill = new Skill(skillId, skillName);
 
-                        if (!skillsCache.containsKey(skillName)) {
-                            skillsCache.put(skillName, newSkill);
-                        }
-
-                        worthOfSkills.put(newSkill, skillWorth);
+                    if (!skillsCache.containsKey(skillName)) {
+                        skillsCache.put(skillName, newSkill);
                     }
 
-                    positionSummary.setWorthOfSkills(worthOfSkills);
-                } catch (SQLException ex) {
-                    System.out.println(ex.getMessage());
+                    worthOfSkills.put(newSkill, skillWorth);
                 }
-            } catch (ClassNotFoundException ex) {
-                System.out.println("There is a problem with driver class!");
-                ex.printStackTrace();
+
+                positionSummary.setWorthOfSkills(worthOfSkills);
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
             }
 
             positionSummariesCache.put(position.name, positionSummary);
             return positionSummary;
         }
     }
-
-
 }
