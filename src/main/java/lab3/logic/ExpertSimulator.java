83eaada5182011fff8484a1b9bcb8485f01483a2
olegSt4 <stefura2010@ukr.net>
2020-11-16 20:37:05 +0200

    feat(ExpertSimulator): additional user prompt added

--- src/main/java/lab3/logic/ExpertSimulator.java
+++ src/main/java/lab3/logic/ExpertSimulator.java
@@ -1,6 +1,13 @@
 package lab3.logic;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 import lab3.controllers.DBController;
+import lab3.controllers.TopPositionsWindowController;
+import lab3.controllers.UserPreferencesWindowController;
 import lab3.models.Position;
 import lab3.models.Skill;
 import lab3.models.PositionSummary;
@@ -14,36 +21,12 @@ public class ExpertSimulator {
         this.dbController = dbController;
     }
 
-    public List<PositionSummary> getTopPositionsBySkillsArray(List<String> skillsNames, int topAmount) {
+    public void showTopPositionsBySkillsArray(List<String> skillsNames, int topAmount) {
         List<Skill> skillsList = dbController.getSkillsListBySkillsNames(skillsNames);
         Set<Position> relevantPositions = dbController.getRelevantPositionsForSkills(skillsList);
-        List<PositionSummary> posSummaries = getPositionSummaries(relevantPositions, skillsList);
+        List<PositionSummary> relevantPosSummaries = getPositionSummaries(relevantPositions, skillsList);
 
-        posSummaries.sort((PositionSummary ss1, PositionSummary ss2) ->
-           Double.compare(ss2.getConformity(), ss1.getConformity())
-        );
-
-
-        List<PositionSummary> topPositions;
-        if (posSummaries.size() > topAmount) {
-            topPositions = posSummaries.subList(0, 3);
-        } else {
-            topPositions = posSummaries.subList(0, posSummaries.size());
-        }
-
-        double currentConformity = 0;
-        for (PositionSummary ss : topPositions) {
-            if (Math.abs(ss.getConformity() - currentConformity) <= 0.1) {
-                System.out.println("There is a match!");
-                // TODO: 12.11.2020 Implement additional user prompt
-                currentConformity = ss.getConformity();
-            } else {
-                currentConformity = ss.getConformity();
-            }
-        }
-
-        return topPositions;
-
+        showTopPositions(relevantPosSummaries, topAmount);
     }
 
     private List<PositionSummary> getPositionSummaries(Set<Position> relevantPositions, List<Skill> skillsList) {
@@ -58,5 +41,82 @@ public class ExpertSimulator {
         }
 
         return posSummaries;
+    }
+
+    private void showTopPositions(List<PositionSummary> relevantPosSummaries, int topAmount) {
+        relevantPosSummaries.sort((PositionSummary ss1, PositionSummary ss2) ->
+                Double.compare(ss2.getConformity(), ss1.getConformity())
+        );
+
+
+        List<PositionSummary> topPositions;
+        if (relevantPosSummaries.size() > topAmount) {
+            topPositions = relevantPosSummaries.subList(0, topAmount);
+        } else {
+            topPositions = relevantPosSummaries.subList(0, relevantPosSummaries.size());
+        }
+
+        if (topPositions.size() < 1) {
+            return;
+        }
+
+        if (topPositions.size() > 1) {
+
+            double topConformity = topPositions.get(0).getConformity();
+            for (PositionSummary topPos : topPositions) {
+                if (topConformity != 1 && Math.abs(topPos.getConformity() - topConformity) <= 0.1) {
+                    askUserPreferences(topPositions);
+                    return;
+                }
+            }
+        }
+
+        drawPositions(topPositions);
+
+
+    }
+
+    public void askUserPreferences(List<PositionSummary> competetivePosSummaries) {
+        try {
+            String fxmlFile = "/fxml/userPreferencesWindow.fxml";
+            FXMLLoader loader = new FXMLLoader();
+            Parent rootNode = loader.load(getClass().getResourceAsStream(fxmlFile));
+
+            Scene scene = new Scene(rootNode);
+
+            Stage stage = new Stage();
+            stage.setTitle("User Preferences");
+            stage.setResizable(false);
+            stage.setScene(scene);
+            stage.show();
+
+            UserPreferencesWindowController UPWC = loader.getController();
+            UPWC.askUser(competetivePosSummaries, dbController);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void drawPositions(List<PositionSummary> topSpecializations) {
+        try {
+            String fxmlFile = "/fxml/topPositionsWindow.fxml";
+            FXMLLoader loader = new FXMLLoader();
+            Parent rootNode = loader.load(getClass().getResourceAsStream(fxmlFile));
+
+            Scene scene = new Scene(rootNode);
+
+            Stage stage = new Stage();
+            stage.setTitle("Top positions");
+            Image icon = new Image("/images/logo.png");
+            stage.getIcons().add(icon);
+            stage.setResizable(false);
+            stage.setScene(scene);
+            stage.show();
+
+            TopPositionsWindowController TPWC = loader.getController();
+            TPWC.showInfo(topSpecializations);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
